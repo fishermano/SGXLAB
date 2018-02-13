18158a7714b347d60695cba5b376863cae1d41c9
CYX <cyx.xjtu@gmail.com>
2018-02-13 11:53:47 -0500

    msg3 generated!

--- Demo/HealthCareProvider/Application/demo_app.c
+++ Demo/HealthCareProvider/Application/demo_app.c
@@ -2,6 +2,7 @@
   Needed for defining integer range, eg. INT_MAX
 */
 #include <limits.h>
+#include <memory.h>
 
 /*
   Needed for untrusted enclave ocall interface
@@ -32,6 +33,10 @@
   Needed to query extended epid group id.
 */
 #include "sgx_uae_service.h"
+
+#ifndef SAFE_FREE
+#define SAFE_FREE(ptr) {if (NULL != (ptr)) {free(ptr); (ptr) = NULL;}}
+#endif
 
 /*
   define the enclave id
@@ -208,7 +213,7 @@ int SGX_CDECL main(int argc, char *argv[]){
       fprintf(OUTPUT, "\nError, call sgx_get_extended_epid_group_id fail [%s].", __FUNCTION__);
       return ret;
     }
-    fprintf(OUTPUT, "\nCall sgx_get_extended_epid_group_id success.");
+    fprintf(OUTPUT, "\nCall sgx_get_extended_epid_group_id success.\n");
 
     p_msg0_full = (ra_samp_request_header_t*) malloc(sizeof(ra_samp_request_header_t) + sizeof(uint32_t));
 
@@ -230,9 +235,7 @@ int SGX_CDECL main(int argc, char *argv[]){
 
     fprintf(OUTPUT, "\nSending msg0 to remote attestation trusted broker.\n");
 
-    ret = ra_network_send_receive("http://DemoTesting.vt.edu",
-        p_msg0_full,
-        &p_msg0_resp_full);
+    ret = ra_network_send_receive("http://demo_testing.cnsr.vt.edu/", p_msg0_full, &p_msg0_resp_full);
     if (ret != 0)
     {
         fprintf(OUTPUT, "\nError, ra_network_send_receive for msg0 failed "
@@ -264,7 +267,218 @@ int SGX_CDECL main(int argc, char *argv[]){
       ret = ecall_init_ra(global_eid, &status, false, &context);
 
     }while(SGX_ERROR_ENCLAVE_LOST == ret && enclave_lost_retry_time--);
+
+    if(SGX_SUCCESS != ret || status)
+    {
+      ret = -1;
+      fprintf(OUTPUT, "\nError, call ecall_init_ra fail [%s].",
+              __FUNCTION__);
+      goto CLEANUP;
+    }
+    fprintf(OUTPUT, "\nCall ecall_init_ra success.\n");
+
   }
+
+  /*
+    msg1
+  */
+  {
+    p_msg1_full = (ra_samp_request_header_t*)
+                  malloc(sizeof(ra_samp_request_header_t) + sizeof(sgx_ra_msg1_t));
+    if(NULL == p_msg1_full){
+      ret = -1;
+      goto CLEANUP;
+    }
+    p_msg1_full->type = TYPE_RA_MSG1;
+    p_msg1_full->size = sizeof(sgx_ra_msg1_t);
+
+    do{
+      ret = sgx_ra_get_msg1(context, global_eid, sgx_ra_get_ga, (sgx_ra_msg1_t*)((uint8_t*)p_msg1_full + sizeof(ra_samp_request_header_t)));
+      sleep(3); // Wait 3s between retries
+    } while (SGX_ERROR_BUSY == ret && busy_retry_time--);
+
+    if(SGX_SUCCESS != ret)
+    {
+      ret = -1;
+      fprintf(OUTPUT, "\nError, call sgx_ra_get_msg1 fail [%s].",
+              __FUNCTION__);
+      goto CLEANUP;
+    }
+    else
+    {
+      fprintf(OUTPUT, "\nCall sgx_ra_get_msg1 success.\n");
+
+      fprintf(OUTPUT, "\nMSG1 body generated -\n");
+
+      PRINT_BYTE_ARRAY(OUTPUT, p_msg1_full->body, p_msg1_full->size);
+    }
+
+    if(VERIFICATION_INDEX_IS_VALID()){
+
+      // memcpy_s(p_msg1_full->body, p_msg1_full->size, msg1_samples[GET_VERIFICATION_ARRAY_INDEX()], p_msg1_full->size);
+      //
+      // fprintf(OUTPUT, "\nInstead of using the recently generated MSG1, "
+      //                 "we will use the following precomputed MSG1 -\n");
+      //
+      // PRINT_BYTE_ARRAY(OUTPUT, p_msg1_full->body, p_msg1_full->size);
+
+    }
+
+
+    // The demo_app sends msg1 to the trusted broker to get msg2,
+    // msg2 needs to be freed when no longer needed.
+    // The demo_app decides whether to use linkable or unlinkable signatures.
+    fprintf(OUTPUT, "\nSending msg1 to remote attestation service provider. Expecting msg2 back.\n");
+
+    ret = ra_network_send_receive("http://demo_testing.cnsr.vt.edu/", p_msg1_full, &p_msg2_full);
+
+    if(ret != 0 || !p_msg2_full){
+      fprintf(OUTPUT, "\nError, ra_network_send_receive for msg1 failed [%s].", __FUNCTION__);
+
+      if(VERIFICATION_INDEX_IS_VALID()){
+        // fprintf(OUTPUT, "\nBecause we are in verification mode we will ignore this error.\n");
+        // fprintf(OUTPUT, "\nInstead, we will pretend we received the following MSG2 - \n");
+        //
+        // SAFE_FREE(p_msg2_full);
+        // ra_samp_response_header_t* precomputed_msg2 =
+        //     (ra_samp_response_header_t*)msg2_samples[
+        //         GET_VERIFICATION_ARRAY_INDEX()];
+        // const size_t msg2_full_size = sizeof(ra_samp_response_header_t)
+        //                               +  precomputed_msg2->size;
+        // p_msg2_full =
+        //     (ra_samp_response_header_t*)malloc(msg2_full_size);
+        // if(NULL == p_msg2_full)
+        // {
+        //     ret = -1;
+        //     goto CLEANUP;
+        // }
+        // memcpy_s(p_msg2_full, msg2_full_size, precomputed_msg2,
+        //          msg2_full_size);
+        //
+        // PRINT_BYTE_ARRAY(OUTPUT, p_msg2_full,
+        //                  sizeof(ra_samp_response_header_t)
+        //                  + p_msg2_full->size);
+      }else{
+        goto CLEANUP;
+      }
+    }else{
+      // Successfully sent msg1 and received a msg2 back.
+      // Time now to check msg2.
+      if(TYPE_RA_MSG2 != p_msg2_full->type){
+
+        fprintf(OUTPUT, "\nError, didn't get MSG2 in response to MSG1. [%s].", __FUNCTION__);
+
+        if(VERIFICATION_INDEX_IS_VALID()){
+          fprintf(OUTPUT, "\nBecause we are in verification mode we will ignore this error.");
+        }
+        else{
+            goto CLEANUP;
+        }
+      }
+
+      fprintf(OUTPUT, "\nSent MSG1 to remote attestation trusted broker. Received the following MSG2:\n");
+      PRINT_BYTE_ARRAY(OUTPUT, p_msg2_full, sizeof(ra_samp_response_header_t) + p_msg2_full->size);
+
+      fprintf(OUTPUT, "\nA more descriptive representation of MSG2:\n");
+      PRINT_ATTESTATION_SERVICE_RESPONSE(OUTPUT, p_msg2_full);
+
+      if( VERIFICATION_INDEX_IS_VALID() )
+      {
+        // The response should match the precomputed MSG2:
+        ra_samp_response_header_t* precomputed_msg2 =
+            (ra_samp_response_header_t *)
+            msg2_samples[GET_VERIFICATION_ARRAY_INDEX()];
+        if(MSG2_BODY_SIZE !=
+            sizeof(ra_samp_response_header_t) + p_msg2_full->size ||
+            memcmp( precomputed_msg2, p_msg2_full,
+                sizeof(ra_samp_response_header_t) + p_msg2_full->size)){
+
+            fprintf(OUTPUT, "\nVerification ERROR. Our precomputed value for MSG2 does NOT match.\n");
+            fprintf(OUTPUT, "\nPrecomputed value for MSG2:\n");
+            PRINT_BYTE_ARRAY(OUTPUT, precomputed_msg2, sizeof(ra_samp_response_header_t) + precomputed_msg2->size);
+            fprintf(OUTPUT, "\nA more descriptive representation of precomputed value for MSG2:\n");
+            PRINT_ATTESTATION_SERVICE_RESPONSE(OUTPUT, precomputed_msg2);
+        }else{
+          fprintf(OUTPUT, "\nVerification COMPLETE. Remote attestation trusted broker generated a matching MSG2.\n");
+        }
+      }
+    }
+  }
+
+  /*
+    msg3
+   */
+  {
+    sgx_ra_msg2_t* p_msg2_body = (sgx_ra_msg2_t*)((uint8_t*)p_msg2_full + sizeof(ra_samp_response_header_t));
+
+    uint32_t msg3_size = 0;
+    if( VERIFICATION_INDEX_IS_VALID())
+    {
+      // // We cannot generate a valid MSG3 using the precomputed messages
+      // // we have been using. We will use the precomputed msg3 instead.
+      // msg3_size = MSG3_BODY_SIZE;
+      // p_msg3 = (sgx_ra_msg3_t*)malloc(msg3_size);
+      // if(NULL == p_msg3)
+      // {
+      //     ret = -1;
+      //     goto CLEANUP;
+      // }
+      // memcpy_s(p_msg3, msg3_size,
+      //          msg3_samples[GET_VERIFICATION_ARRAY_INDEX()], msg3_size);
+      // fprintf(OUTPUT, "\nBecause MSG1 was a precomputed value, the MSG3 "
+      //                 "we use will also be. PRECOMPUTED MSG3 - \n");
+    }else{
+      busy_retry_time = 2;
+      // The demo_app now calls uKE sgx_ra_proc_msg2,
+      // The demo_app is responsible for freeing the returned p_msg3!!
+      do
+      {
+        ret = sgx_ra_proc_msg2(context,
+                           global_eid,
+                           sgx_ra_proc_msg2_trusted,
+                           sgx_ra_get_msg3_trusted,
+                           p_msg2_body,
+                           p_msg2_full->size,
+                           &p_msg3,
+                           &msg3_size);
+      }while(SGX_ERROR_BUSY == ret && busy_retry_time--);
+
+      if(!p_msg3){
+        fprintf(OUTPUT, "\nError, call sgx_ra_proc_msg2 fail. p_msg3 = 0x%p [%s].", p_msg3, __FUNCTION__);
+        ret = -1;
+        goto CLEANUP;
+      }
+
+      if(SGX_SUCCESS != (sgx_status_t)ret){
+        fprintf(OUTPUT, "\nError, call sgx_ra_proc_msg2 fail. ret = 0x%08x [%s].", ret, __FUNCTION__);
+        ret = -1;
+        goto CLEANUP;
+      }else{
+        fprintf(OUTPUT, "\nCall sgx_ra_proc_msg2 success.\n");
+        fprintf(OUTPUT, "\nMSG3 - \n");
+      }
+    }
+
+    PRINT_BYTE_ARRAY(OUTPUT, p_msg3, msg3_size);
+
+    // p_msg3_full = (ra_samp_request_header_t*)malloc(
+    //                sizeof(ra_samp_request_header_t) + msg3_size);
+    // if(NULL == p_msg3_full)
+    // {
+    //   ret = -1;
+    //   goto CLEANUP;
+    // }
+    // p_msg3_full->type = TYPE_RA_MSG3;
+    // p_msg3_full->size = msg3_size;
+    // if(memcpy_s(p_msg3_full->body, msg3_size, p_msg3, msg3_size)){
+    //   fprintf(OUTPUT,"\nError: INTERNAL ERROR - memcpy failed in [%s].", __FUNCTION__);
+    //   ret = -1;
+    //   goto CLEANUP;
+    // }
+
+  }
+
+
 
   printf("***Remote Attestation Functionality***\n");
   ecall_verify_att_result_mac(global_eid);
