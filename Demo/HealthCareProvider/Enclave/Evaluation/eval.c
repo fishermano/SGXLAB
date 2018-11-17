2867f1142516a084d9da3b384a1fa0217d986dac
CYX <cyx.xjtu@gmail.com>
2018-11-17 07:48:50 -0500

    final update

--- Demo/HealthCareProvider/Enclave/Evaluation/eval.c
+++ Demo/HealthCareProvider/Enclave/Evaluation/eval.c
@@ -9,7 +9,7 @@
 
 extern uint8_t shared_key[16];
 
-#define DATA_SIZE 1
+#define DATA_SIZE 10000
 
 #pragma pack(1)
 struct file_t {
@@ -28,28 +28,28 @@ uint8_t sk[16] = {
 
 sgx_status_t ecall_evaluate_decryption(uint8_t *p_files, uint32_t file_number, uint32_t total_size){
 
-  ocall_print("testing enclave function: ecall_evaluate_decryption()");
-  ocall_print_int(file_number);
-  ocall_print_int(total_size);
+  // ocall_print("testing enclave function: ecall_evaluate_decryption()");
+  // ocall_print_int(file_number);
+  // ocall_print_int(total_size);
 
   sgx_status_t ret = SGX_SUCCESS;
 
-  uint8_t output[DATA_SIZE];
+  uint8_t output[DATA_SIZE] = {0};
   uint8_t aes_gcm_iv[12] = {0};
 
   struct file_t *file = (struct file_t *)malloc(DATA_SIZE + 20);
 
   for(uint32_t i = 0; i < file_number; i++){
-    ocall_print("$$$$$$$$$$$$");
+    // ocall_print("$$$$$$$$$$$$");
     memcpy(file, p_files, DATA_SIZE + 20);
-    ocall_print("++++++++++++");
-    ret = sgx_rijndael128GCM_decrypt(&sk, file->payload, file->payload_size, &output[0], &aes_gcm_iv[0], 12, NULL, 0, (const sgx_aes_gcm_128bit_tag_t *)(file->payload_tag));
+    // ocall_print("++++++++++++");
+    ret = sgx_rijndael128GCM_decrypt(&shared_key, file->payload, file->payload_size, &output[0], &aes_gcm_iv[0], 12, NULL, 0, (const sgx_aes_gcm_128bit_tag_t *)(file->payload_tag));
     // for(uint32_t j = 0; j < DATA_SIZE + 20; j++){
     //   ocall_print_int(*(&file->payload_tag[0] + j));
     // }
-    ocall_print("------------");
+    // ocall_print("------------");
     p_files = p_files + DATA_SIZE + 20;
-    ocall_print("************");
+    // ocall_print("************");
 
   }
 
